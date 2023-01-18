fa03f44ea5d16bd25824a4456dc7ca8c49b054dc
JimHwkins <howlarrior@gmail.com>
2023-01-18 19:06:47 -0300

    Assignment Completed

--- input.js
+++ input.js
@@ -1,6 +1,7 @@
 let connection;
 
-const setupInput = () => {
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -12,7 +13,29 @@ const setupInput = () => {
 
 const handleUserInput = () => {
   process.stdin.on("data", (key) => {
-    if (key === "\u0003") {
+    switch (key) {
+    case "w":
+      connection.write("Move: up");
+      break;
+
+    case "a":
+      connection.write("Move: right");
+      break;
+
+    case "s":
+
+      connection.write("Move: down");
+      break;
+
+    case "d":
+      connection.write("Move: left");
+      break;
+
+    case "x":
+      connection.write('Say: Hi Mom!');
+      break;
+
+    case "\u0003":
       process.exit();
     }
   });
